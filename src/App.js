f0714b13208792ffa91c97f1d50710e44bae5f63
ompgupta <68551688+ompgupta@users.noreply.github.com>
2022-01-03 12:14:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import PhoneComp from './components/PhoneComp'
 import './App.css';
+import {FaUserCircle} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <h1 className='text-3xl'>Helllo</h1> */}
+    {/* <FaUserCircle/> */}
+    <PhoneComp/>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'phone-img': "url('/public/iphoneX-logo.png')",
+      }
+    },
+  },
+  plugins: [],
+}
